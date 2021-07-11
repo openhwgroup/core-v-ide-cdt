b31f85814131e922ddab5575f9755c3daa21700b
Nikifor Fedorov <zelenyhleb@gmail.com>
2021-07-11 13:02:42 +0300

    Created custom CORE-V project wizard for further development
    Signed-off-by: Nikifor Fedorov <zelenyhleb@gmail.com>

---
+++ bundles/org.openhwgroup.corev.ide.definition.ui/src/org/openhwgroup/corev/ide/definition/ui/DefinePage.java
@@ -0,0 +1,121 @@
+/*******************************************************************************
+ * Copyright (c) 2021 ArSysOp and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Nikifor Fedorov (ArSysOp) - initial API and implementation
+ *******************************************************************************/
+package org.openhwgroup.corev.ide.definition.ui;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Text;
+import org.openhwgroup.corev.ide.definition.api.Configuration;
+import org.openhwgroup.corev.ide.definition.project.EmptyConfiguration;
+
+public final class DefinePage extends WizardPage {
+
+	private String projectName = new String();
+	private String projectPath = new String();
+
+	protected DefinePage() {
+		super("definition"); //$NON-NLS-1$
+		setDescription("Create new CORE-V project from scratch"); //$NON-NLS-1$
+		setTitle("New CORE-V project"); //$NON-NLS-1$
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		Composite container = container(parent);
+		createNameGroup(container);
+		createPathGroup(container);
+		createConfigurationGroup(container);
+		setControl(container);
+	}
+
+	private Composite container(Composite parent) {
+		Composite container = new Composite(parent, SWT.NONE);
+		container.setLayout(GridLayoutFactory.swtDefaults().numColumns(3).create());
+		container.setLayoutData(GridDataFactory.fillDefaults().create());
+		return container;
+	}
+
+	private void createNameGroup(Composite container) {
+		Label label = new Label(container, SWT.NONE);
+		label.setLayoutData(GridDataFactory.fillDefaults().create());
+		label.setText("Project name:"); //$NON-NLS-1$
+		Text name = new Text(container, SWT.BORDER);
+		name.setMessage("Project name"); //$NON-NLS-1$
+		name.setLayoutData(GridDataFactory.fillDefaults().span(2, 1).grab(true, false).create());
+		name.addModifyListener(event -> {
+			if (!ResourcesPlugin.getWorkspace().getRoot().getProject(name.getText()).exists()) {
+				projectName = name.getText();
+			}
+		});
+	}
+
+	private void createPathGroup(Composite container) {
+		Label pathLabel = new Label(container, SWT.NONE);
+		pathLabel.setLayoutData(GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).create());
+		pathLabel.setText("Path to the project root:"); //$NON-NLS-1$
+		Text path = new Text(container, SWT.BORDER);
+		path.setMessage("Path to the project root"); //$NON-NLS-1$
+		path.setLayoutData(GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).grab(true, false).create());
+		path.addModifyListener(event -> {
+			projectPath = path.getText();
+		});
+		Button pathButton = new Button(container, SWT.PUSH);
+		pathButton.setLayoutData(GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).create());
+		pathButton.setText("Browse..."); //$NON-NLS-1$
+		pathButton.addListener(SWT.Selection, event -> {
+			DirectoryDialog dialog = new DirectoryDialog(container.getShell(), SWT.NONE);
+			String open = dialog.open();
+			path.setText(open);
+			projectPath = open;
+		});
+	}
+
+	private void createConfigurationGroup(Composite container) {
+		Group configuration = new Group(container, SWT.NONE);
+		configuration.setText("Project configuration"); //$NON-NLS-1$
+		configuration.setLayout(GridLayoutFactory.swtDefaults().numColumns(2).equalWidth(true).create());
+		configuration.setLayoutData(GridDataFactory.fillDefaults().span(3, 1).grab(true, true).create());
+		List boards = new List(configuration, SWT.BORDER);
+		boards.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
+		List toolchains = new List(configuration, SWT.BORDER);
+		toolchains.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
+		Button boardsButton = new Button(configuration, SWT.PUSH);
+		boardsButton.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).create());
+		boardsButton.setText("Select boards"); //$NON-NLS-1$
+		Button toolchainsButton = new Button(configuration, SWT.PUSH);
+		toolchainsButton.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).create());
+		toolchainsButton.setText("Select toolchains"); //$NON-NLS-1$
+	}
+
+	public String name() {
+		return projectName;
+	}
+
+	public String path() {
+		return projectPath;
+	}
+
+	public Configuration configuration() {
+		return new EmptyConfiguration();
+	}
+
+}
---
+++ bundles/org.openhwgroup.corev.ide.definition/src/org/openhwgroup/corev/ide/definition/project/DefinedToolchain.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2021 ArSysOp and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Nikifor Fedorov (ArSysOp) - initial API and implementation
+ *******************************************************************************/
+package org.openhwgroup.corev.ide.definition.project;
+
+import java.util.List;
+
+import org.openhwgroup.corev.ide.definition.api.Board;
+import org.openhwgroup.corev.ide.definition.api.Toolchain;
+
+public final class DefinedToolchain implements Toolchain {
+
+	private final String identifier;
+	private final String title;
+	private final String path;
+	private final List<Board> boards;
+
+	public DefinedToolchain(String identifier, String title, String path, List<Board> boards) {
+		this.identifier = identifier;
+		this.title = title;
+		this.path = path;
+		this.boards = boards;
+	}
+
+	@Override
+	public String identifier() {
+		return identifier;
+	}
+
+	@Override
+	public String title() {
+		return title;
+	}
+
+	@Override
+	public String path() {
+		return path;
+	}
+
+	@Override
+	public List<Board> boards() {
+		return boards;
+	}
+
+}
